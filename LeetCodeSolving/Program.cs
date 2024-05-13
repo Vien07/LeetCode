fb049b991c89a12a62be3b3b1e1f1b210ada7b20
Luka Redgrave <viendinhduy@gmail.com>
2024-05-13 17:37:20 +0700

    update

--- LeetCodeSolving/Program.cs
+++ LeetCodeSolving/Program.cs
@@ -70,10 +70,84 @@ public class LeetCode
 
         return true;
     }
-    // 3.Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    public bool IsAnagramBest(string s, string t)
+    {
+        var dictS = new Dictionary<char, int>();
+        var dictT = new Dictionary<char, int>();
+
+        if (s.Length != t.Length) return false;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (dictS.ContainsKey(s[i]))
+            {
+                dictS[s[i]]++;
+            }
+            else
+            {
+                dictS.Add(s[i], 1);
+            }
+            if (dictT.ContainsKey(t[i]))
+            {
+                dictT[t[i]]++;
+            }
+            else
+            {
+                dictT.Add(t[i], 1);
+
+            }
+        }
+        foreach (var d in dictS)
+        {
+            if (!(dictT.ContainsKey(d.Key) && dictT[d.Key] == d.Value)) return false;
+        }
+        return true;
+
+    }
+    // 3. Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
     //You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
     //You can return the answer in any order.
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> indexNum = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var compliment = target - nums[i];
+            if (indexNum.ContainsKey(compliment))
+            {
+                return new int[] { i, indexNum[compliment] };
+            }
+            indexNum[nums[i]] = i;
+        }
+        return null;
+    }
+    // 4. Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        var groupedAnagrams = new Dictionary<string, IList<string>>();
 
+        foreach (string str in strs)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Sort(charArray);
+            string sortedStr = new string(charArray);
+
+            if (!groupedAnagrams.ContainsKey(sortedStr))
+            {
+                groupedAnagrams[sortedStr] = new List<string>();
+            }
+
+            groupedAnagrams[sortedStr].Add(str);
+        }
+
+        return groupedAnagrams.Values.ToList();
+    }
+    // 5. Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+
+        return null;
+    }
 }
--- LeetCodeSolving/obj/Debug/net8.0/LeetCodeSolving.AssemblyInfo.cs
+++ LeetCodeSolving/obj/Debug/net8.0/LeetCodeSolving.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCodeSolving")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+775fa030388efdba234a744a2fb83448d163bd59")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCodeSolving")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCodeSolving")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
